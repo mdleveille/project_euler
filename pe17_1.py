fffdb1e0a13f110eabc851d31da144a038d2a346
mleveille <mleveille@stsci.edu>
2019-02-11 09:21:31 -0500

    work on 17

--- pe17_1.py
+++ pe17_1.py
@@ -7,4 +7,6 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 """
 
-number_length = {0:0, 1:3, 2:3, 3:5 , 4: , 5: , 6: , 7: , 8: , 9: , 3: ,}
+ones_length = {0:0, 1:3, 2:3, 3:5 , 4:4 , 5:4 , 6:3 , 7:5 , 8:5 , 9:4}
+
+tens_length = {0:0, 1:3, 2:3, 3:5 , 4:4 , 5:4 , 6:3 , 7:5 , 8:5 , 9:4}
